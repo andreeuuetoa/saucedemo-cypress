ca5e6cb18ec2affd4c60d486651507c267cf39c5
andreeuuetoa <andree.uuetoa@gmail.com>
2024-01-29 01:15:15 +0200

    The Swag Labs title is on the home page

--- cypress/e2e/smoke/homepage.cy.ts
+++ cypress/e2e/smoke/homepage.cy.ts
@@ -1,5 +1,7 @@
-describe('template spec', () => {
+describe('Home page test', () => {
   it('passes', () => {
     cy.visit('https://www.saucedemo.com/')
+
+    cy.get(".login_logo").contains("Swag Labs");
   })
 })
