96d23c414b922191a233251eb8ca83b9537f2cd6
andreeuuetoa <andree.uuetoa@gmail.com>
2024-01-29 01:21:57 +0200

    Test that the log-in form is on the home page

--- cypress/e2e/smoke/homepage.cy.ts
+++ cypress/e2e/smoke/homepage.cy.ts
@@ -5,3 +5,11 @@ describe('Home page test', () => {
     cy.get(".login_logo").contains("Swag Labs");
   })
 })
+
+describe('Log in from home page', () => {
+  it('checks for log-in form on the home page', () => {
+    cy.visit('https://www.saucedemo.com');
+
+    cy.get('#user-name').type('standard_user');
+  })
+});
