3e094b58e5a32737ac558a12d0c5d7c76005ad29
andreeuuetoa <andree.uuetoa@gmail.com>
2024-01-29 13:59:31 +0200

    Add test for an unsuccessful login

--- cypress/e2e/page/loginpage.cy.ts
+++ cypress/e2e/page/loginpage.cy.ts
@@ -1,4 +1,8 @@
-describe('Successful login', () => {
+describe('Login tests', () => {
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com');
+  });
+
   it('Shows the products page', () => {
     cy.visit('https://www.saucedemo.com/');
 
@@ -7,5 +11,16 @@ describe('Successful login', () => {
     cy.get('#login-button').click();
 
     cy.get('.title').contains('Products');
+  })
+
+  it('Shows the error message', () => {
+    cy.visit('https://www.saucedemo.com');
+
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('wrong_password');
+    cy.get('#login-button').click();
+
+    const errorMessage = 'Epic sadface: Username and password do not match any user in this service';
+    cy.get('.error-message-container').contains(errorMessage);
   })
 });
